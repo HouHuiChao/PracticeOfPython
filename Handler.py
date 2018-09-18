b71ddcc082c00ac4ce6a01584176d11d044fd962
HouHuiChao <1822432547@qq.com>
2018-09-18 11:25:49 +0800

    Handler.py

--- Handler.py
+++ Handler.py
@@ -9,9 +9,9 @@ class handler(object):
 		super(handler, self).__init__()
 		self.cmd = arg
 	def CmdHandle():
-		f = open('./log.txt','a+',encoding='utf-8')
-		timeStr = datetime.datetime.strftime(time1,'%Y-%m-%d %H:%M:%S')
-		logStr = timeStr + self.cmd
+		f = open('./log.txt','a+')
+		timeStr = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+		logStr = timeStr + "\t" + self.cmd + "\n"
 		print logStr
 		f.write(logStr)
 		f.close()
